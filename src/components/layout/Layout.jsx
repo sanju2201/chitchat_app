48a0740e611fdc036a74e2647b5387a3cc2d6c40
sangeeta <shrivassangeeta57@gmail.com>
2023-03-24 17:34:11 +0530

    First commit

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Jobs from "../pages/jobs/Jobs";
 
 const Layout = () => {
-  return <div>Layout of Application</div>;
-};
-
+  return (
+    <div>
+      <div>Layout of Application</div>
+      <Jobs></Jobs>
+    </div>
+  );
+  }
 export default Layout;
