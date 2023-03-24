816cec5e3ff4763903a8ff66761e3d663be9e3ee
KM31RITU <remix31rj@gmail.com>
2023-03-24 18:13:46 +0530

    first commit

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Login from "../pages/loginPanel/login";
 const Layout = () => {
   return <div>Layout of Application</div>;
+  <div>
+<div>Layout of Application</div>
+<Login> </Login>
+  </div>
 };
 
 export default Layout;
